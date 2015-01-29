e511fe8eec0ed7711a93092eb58fd3691acd6fb7
alreyes1 <alreyes1@icloud.com>
2015-01-28 22:45:19 -0500

    Added hover class to menu links

--- jQuery/main.js
+++ jQuery/main.js
@@ -4,7 +4,18 @@ $(document).on('ready', function(){
 	  $('.slideDownMenu').slideToggle("slow");
 
 	});
-	
+	$('.home').hover(function(){
+		$('.home').toggleClass('hoverCSS');
+	});
+	$('.projects').hover(function(){
+		$('.projects').toggleClass('hoverCSS');
+	});
+	$('.hobbies').hover(function(){
+		$('.hobbies').toggleClass('hoverCSS');
+	});
+	$('.resume').hover(function(){
+		$('.resume').toggleClass('hoverCSS');
+	});
 });
 
 
