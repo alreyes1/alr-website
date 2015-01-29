7217dfc7c0ce058fab9b042bac28fb5537c91692
alreyes1 <alreyes1@icloud.com>
2015-01-28 22:19:56 -0500

    First Commit

---
+++ jQuery/main.js
@@ -0,0 +1,8 @@
+$(document).on('ready', function(){
+	$('.glyphicon-menu-hamburger').click(function(){
+	  // $('.slideDownMenu').toggleClass('displaymenu');
+	  $('.slideDownMenu').slideToggle("slow");
+	});
+});
+
+
